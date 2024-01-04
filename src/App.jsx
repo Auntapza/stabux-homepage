4ea3418fa44385f592cb5ff8b11794179f3a690c
Auntapza <chatdanai.k7563@gmail.com>
2024-01-04 23:39:46 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./component/navbar"
+
+let App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../assets/starbucks_corporation_logo.png"
+
+let navbar = () => {
+    return (
+        <>
+            <nav>
+                <div className="container">
+                    <div className="left">
+                        <img src={logo} alt="" />
+                        <div className="nav-item">
+                            <ul>
+                                <li>coffee tea at home</li>
+                                <li>menu</li>
+                                <li>merchandice</li>
+                                <li>starbucks rewards</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <a href="">
+                            <i className="fa-solid fa-location-dot"></i>
+                            Find a Store
+                        </a>
+                        <a href="">
+                            <i class="fa-solid fa-flag"></i>
+                            Thai
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default navbar
