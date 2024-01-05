974f4ebfd501a5455531bfafb13654590748f077
Auntapza <chatdanai.k7563@gmail.com>
2024-01-05 12:24:07 +0700

    sliderImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./component/navbar"
+import HomePage from "./component/homepage/homepage"
 import "./App.css"
 
 let App = () => {
   return (
     <>
       <Navbar />
+      <HomePage />
     </>
   )
 }
---
+++ src/component/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ImageSlider from "./imageSlider"
+
+let hompage = () => {
+    return (
+        <>
+            <ImageSlider />
+        </>
+    )
+}
+
+export default hompage
